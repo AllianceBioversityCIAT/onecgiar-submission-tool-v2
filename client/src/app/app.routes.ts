1ff06b2f7c89c013a9daf95d9b9fabe04f26536e
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2024-01-23 10:36:32 -0500

    ✨ feat(project): login page added

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login/:awsToken',
+    loadComponent: () =>
+      import('@auth/login/login.component').then((c) => c.LoginComponent),
+  },
+];
---
+++ client/src/app/domain/auth/login/login.component.ts
@@ -0,0 +1,18 @@
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+})
+export class LoginComponent {
+  private activatedRoute = inject(ActivatedRoute);
+
+  constructor() {
+    const awsToken = this.activatedRoute.snapshot.paramMap.get('awsToken');
+    console.log(awsToken);
+  }
+}
