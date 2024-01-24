adf8ee8f9555d12ee25de13f63fe2297b4b05034
Kevin Collazos <kevcollazos@gmail.com>
2024-01-24 16:12:03 -0500

    :sparkles: feat(footer): add footer component and some navbar style changes

--- client/src/app/domain/initiatives/components/footer/footer.component.ts
+++ client/src/app/domain/initiatives/components/footer/footer.component.ts
@@ -5,8 +5,21 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
-
+  footerLinks = [
+    {
+      label: 'Contact Us',
+      link: '/about',
+    },
+    {
+      label: 'Terms and Conditions',
+      link: 'https://www.notion.so/Legal-Terms-eb2f3e436f3e46bd9057b1cb1e3391b8',
+    },
+    {
+      label: 'License',
+      link: 'https://github.com/AllianceBioversityCIAT/onecgiar-submission-tool/blob/main/LICENSE',
+    },
+  ];
 }
