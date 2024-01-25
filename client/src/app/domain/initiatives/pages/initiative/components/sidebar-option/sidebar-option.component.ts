c4458e90dc84057dcffc3b7e578a02e5f8f52c02
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2024-01-25 10:38:49 -0500

    :sparkles: feat(sidebar) P2-212: animate sidebar option expand

--- client/src/app/domain/initiatives/pages/initiative/components/sidebar-option/sidebar-option.component.ts
+++ client/src/app/domain/initiatives/pages/initiative/components/sidebar-option/sidebar-option.component.ts
@@ -1,3 +1,10 @@
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component, Input } from '@angular/core';
 
 @Component({
@@ -6,6 +13,14 @@ import { Component, Input } from '@angular/core';
   imports: [],
   templateUrl: './sidebar-option.component.html',
   styleUrl: './sidebar-option.component.scss',
+  animations: [
+    trigger('expand', [
+      state('expand_more', style({ height: '*' })),
+      state('expand_less', style({ height: '0' })),
+      transition('expand_less => expand_more', animate('.3s ease-in')),
+      transition('expand_more => expand_less', animate('.3s ease-out')),
+    ]),
+  ],
 })
 export class SidebarOptionComponent {
   @Input() option: any;
