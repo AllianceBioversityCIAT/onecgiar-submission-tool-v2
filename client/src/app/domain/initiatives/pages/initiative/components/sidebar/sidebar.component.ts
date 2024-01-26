aabaa790775076f81a7295116e61720745431d08
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2024-01-26 09:47:39 -0500

    :sparkles: feat(styles) P2-212: dynamic routing added

--- client/src/app/domain/initiatives/pages/initiative/components/sidebar/sidebar.component.ts
+++ client/src/app/domain/initiatives/pages/initiative/components/sidebar/sidebar.component.ts
@@ -22,67 +22,88 @@ import { InitiativeService } from '../../services/initiative.service';
 export class SidebarComponent {
   router = inject(Router);
   initiativeSE = inject(InitiativeService);
-  options: Signal<SidebarOption[]> = computed(() => [
-    {
-      label: 'Overview',
-      route: this.initiativeSE.currentInitiativeId() + 'overview',
-      inUnderConstruction: false,
-    },
-    {
-      label: '2. Context',
-      children: [
-        {
-          label: '2.1 Challenge Statement',
-          route: 'overview',
-        },
-        {
-          label: '2.2 Measurable three-year (End of Initiative) outcomes',
-          route: 'overview',
-        },
-        {
-          label: '2.3 Comparative Advantage',
-          route: 'overview',
-          children: [
-            {
-              label: '2.3.1 Incentives',
-            },
-            {
-              label: '2.3.2 Human capital',
-            },
-            {
-              label: '2.3.3 Biophysical capital',
-            },
-            {
-              label: '2.3.4 Social capital',
-            },
-          ],
-        },
-        {
-          label: '2.4 Partnerships',
-          route: 'overview',
-        },
-        {
-          label:
-            '2.5 Learning from prior evaluations and Impact Assessments (IA)',
-          route: 'overview',
-        },
-        {
-          label: '2.6 Priority setting',
-          route: 'overview',
-        },
-        {
-          label: '2.7 Participatory design process',
-          route: 'overview',
-        },
-        {
-          label: '2.8 Portfolio Linkages',
-          route: 'overview',
-        },
-      ],
-    },
-  ]);
+  options: Signal<SidebarOption[]> = computed(() => {
+    // this.initiativeSE.currentInitiativeId() +
+    let options = [
+      {
+        label: 'Overview',
+        route: 'overview',
+        inUnderConstruction: false,
+      },
+      {
+        label: '2. Context',
+        route: 'context',
+        children: [
+          {
+            label: '2.1 Challenge Statement',
+            route: 'ChallengeStatement',
+          },
+          {
+            label: '2.2 Measurable three-year (End of Initiative) outcomes',
+            route: 'overview',
+          },
+          {
+            label: '2.3 Comparative Advantage',
+            route: 'overview',
+            children: [
+              {
+                label: '2.3.1 Incentives',
+              },
+              {
+                label: '2.3.2 Human capital',
+              },
+              {
+                label: '2.3.3 Biophysical capital',
+              },
+              {
+                label: '2.3.4 Social capital',
+              },
+            ],
+          },
+          {
+            label: '2.4 Partnerships',
+            route: 'overview',
+          },
+          {
+            label:
+              '2.5 Learning from prior evaluations and Impact Assessments (IA)',
+            route: 'overview',
+          },
+          {
+            label: '2.6 Priority setting',
+            route: 'overview',
+          },
+          {
+            label: '2.7 Participatory design process',
+            route: 'overview',
+          },
+          {
+            label: '2.8 Portfolio Linkages',
+            route: 'overview',
+          },
+        ],
+      },
+    ];
 
-  chageee() {
-    this.initiativeSE.currentInitiativeId.set('2');
-  }
+    // agrega el id de la iniciativa a las rutas al parametro route, pero si tine hijas pones el del padre y si tiene otro nivel haz lo mismo
+    const addInitiativeId = (options: SidebarOption[], parentPath: string) => {
+      options.forEach((option) => {
+        if (option.route) {
+          option.route = `${parentPath}/${option.route}`;
+        }
+        if (option.children) {
+          addInitiativeId(option.children, option?.route ?? '');
+        }
+      });
+    };
+
+    addInitiativeId(
+      options,
+      `initiative/${this.initiativeSE.currentInitiativeId()}`,
+    );
+
+    console.log(options);
+
+    return options;
+  });
 }
