fd76bbf014864b13071e9bee7343836cd0fab5e3
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2024-01-29 11:43:47 -0500

    :sparkles: feat(environments) P2-212: configured environment files

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,11 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'client';
+  constructor() {
+    console.warn(
+      `%c Environment:%c${environment.envName.charAt(0).toUpperCase()}${environment.envName.slice(1)} `,
+      'background: #ffffff; color: #212121; font-size: 16px; padding: 5px; font-weight: bold',
+      'background: #ffffff; color: #326fd1; font-size: 16px; padding: 5px; font-weight: bold',
+    );
+  }
 }
