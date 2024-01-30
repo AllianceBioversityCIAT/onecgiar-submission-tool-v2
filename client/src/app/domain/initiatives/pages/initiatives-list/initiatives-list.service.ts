0361cd92377979bcc2ea11f984e260d33f71a83a
xKeCo <kevcollazos@gmail.com>
2024-01-30 07:24:09 -0500

    :recycle: refactor(Initiative List): update global filter fields in initiatives list and remove commented code

--- client/src/app/domain/initiatives/pages/initiatives-list/initiatives-list.service.ts
+++ client/src/app/domain/initiatives/pages/initiatives-list/initiatives-list.service.ts
@@ -5,24 +5,6 @@ import { HttpClient } from '@angular/common/http';
 export class InitiativesService {
   getData() {
     return [
-      // {
-      //   id: 1000,
-      //   name: 'James Butt',
-      //   country: {
-      //     name: 'Algeria',
-      //     code: 'dz',
-      //   },
-      //   company: 'Benton, John B Jr',
-      //   date: '2015-09-13',
-      //   status: 'unqualified',
-      //   verified: true,
-      //   activity: 17,
-      //   representative: {
-      //     name: 'Ioni Bowcher',
-      //     image: 'ionibowcher.png',
-      //   },
-      //   balance: 70663,
-      // },
       {
         acronym: 'Accelerated Breeding',
         action_area_description: 'Genetic Innovation',
