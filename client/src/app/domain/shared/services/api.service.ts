6a8ee19ccf256eae779bcd7ebd3608918f95708d
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2024-02-06 09:51:25 -0500

    :sparkles: feat(overview) P2-212: overview endpoints added

--- client/src/app/domain/shared/services/api.service.ts
+++ client/src/app/domain/shared/services/api.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, inject } from '@angular/core';
 import { ToPromiseService } from './to-promise.service';
 import { ErrorResponse, LoginRes, MainResponse } from '../interfaces/responses.interface';
+import { OverviewBody } from '../models/overview-body.class';
 
 @Injectable({
   providedIn: 'root',
@@ -8,13 +9,18 @@ import { ErrorResponse, LoginRes, MainResponse } from '../interfaces/responses.i
 export class ApiService {
   TP = inject(ToPromiseService);
 
-  getExample = () => {
-    const url = () => 'https://jsonplaceholder.typicode.com/users';
-    return this.TP.get(url());
-  };
-
   login = (awsToken: string): Promise<MainResponse<LoginRes>> => {
     const url = () => `http://localhost:3002/auth/login`;
     return this.TP.post(url(), {}, awsToken);
+  };
+
+  GET_overview = (id: string): Promise<MainResponse<OverviewBody>> => {
+    const url = () => `api/entity/${id}/overview-summary`;
+    return this.TP.get(url(), true);
+  };
+
+  PATCH_overview = (id: string, body: OverviewBody): Promise<MainResponse<OverviewBody>> => {
+    const url = () => `api/entity/${id}/overview-summary/save`;
+    return this.TP.patch(url(), body);
   };
 }
