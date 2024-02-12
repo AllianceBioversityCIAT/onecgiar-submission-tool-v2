60d1b28131d87fbe2e77436d61904ea66e8c0194
xKeCo <kevcollazos@gmail.com>
2024-02-12 14:50:36 -0500

    :white_check_mark: test(section 5) P2-216: added unit test for section 5 component

--- client/src/app/domain/initiatives/pages/initiative/pages/gender/gender.component.spec.ts
+++ client/src/app/domain/initiatives/pages/initiative/pages/gender/gender.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GenderComponent } from './gender.component';
+import { FormsModule } from '@angular/forms';
+import { EditorModule } from 'primeng/editor';
+import { ButtonModule } from 'primeng/button';
+import { FieldContainerDirective } from '../../../../../shared/directives/field-container.directive';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('GenderComponent', () => {
   let component: GenderComponent;
@@ -8,7 +13,14 @@ describe('GenderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [GenderComponent],
+      imports: [
+        FormsModule,
+        EditorModule,
+        ButtonModule,
+        GenderComponent,
+        FieldContainerDirective,
+        HttpClientTestingModule,
+      ],
     }).compileComponents();
   });
 
@@ -22,19 +34,79 @@ describe('GenderComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should call saveGenderResearchAndImpactSection after 1500ms', () => {
-    jest.useFakeTimers();
-    const saveGenderResearchAndImpactSectionSpy = jest.spyOn(
-      component,
-      'saveGenderResearchAndImpactSection',
+  it('should initialize genderResearchAndImpactBody with null value', () => {
+    expect(component.genderResearchAndImpactBody().gender_research_impact_p25_html).toBeNull();
+  });
+
+  it('should call getGenderResearchAndImpact on ngOnInit', () => {
+    jest.spyOn(component, 'getGenderResearchAndImpact');
+    component.ngOnInit();
+    expect(component.getGenderResearchAndImpact).toHaveBeenCalled();
+  });
+
+  it('should set gender_research_impact_p25_html when getGenderResearchAndImpact is called and response is successful', async () => {
+    const mockResponse = {
+      success: true,
+      data: {
+        data: {
+          gender_research_impact_p25_html: '<p>Gender research impact HTML</p>',
+        },
+      },
+    };
+    jest.spyOn(component.api, 'GET_GenderResearchAndImpact').mockResolvedValue(mockResponse);
+    await component.getGenderResearchAndImpact();
+    expect(component.genderResearchAndImpactBody().gender_research_impact_p25_html).toBe(
+      '<p>Gender research impact HTML</p>',
     );
+  });
 
-    expect(saveGenderResearchAndImpactSectionSpy).not.toHaveBeenCalled();
+  it('should not set gender_research_impact_p25_html when getGenderResearchAndImpact is called and response is not successful', async () => {
+    const mockResponse = {
+      data: null,
+      success: false,
+    };
+    jest.spyOn(component.api, 'GET_GenderResearchAndImpact').mockResolvedValue(mockResponse);
+    await component.getGenderResearchAndImpact();
+    expect(component.genderResearchAndImpactBody().gender_research_impact_p25_html).toBeNull();
+  });
 
-    component.saveGenderResearchAndImpactSection();
+  it('should call PATCH_GenderResearchAndImpact and display success message when saveGenderResearchAndImpactSection is called and response is successful', async () => {
+    const mockResponse = {
+      data: null,
+      success: true,
+    };
+    jest.spyOn(component.api, 'PATCH_GenderResearchAndImpact').mockResolvedValue(mockResponse);
+    jest.spyOn(component.messageService, 'add');
+    jest.spyOn(component.globalVars.isSavingSection, 'set');
+    await component.saveGenderResearchAndImpactSection();
+    expect(component.api.PATCH_GenderResearchAndImpact).toHaveBeenCalledWith(
+      component.genderResearchAndImpactBody(),
+    );
+    expect(component.messageService.add).toHaveBeenCalledWith({
+      severity: 'success',
+      summary: 'Success',
+      detail: 'Success',
+    });
+    expect(component.globalVars.isSavingSection.set).toHaveBeenCalledWith(false);
+  });
 
-    jest.advanceTimersByTime(1500);
-
-    expect(saveGenderResearchAndImpactSectionSpy).toHaveBeenCalled();
+  it('should call PATCH_GenderResearchAndImpact and display error message when saveGenderResearchAndImpactSection is called and response is not successful', async () => {
+    const mockResponse = {
+      data: null,
+      success: false,
+    };
+    jest.spyOn(component.api, 'PATCH_GenderResearchAndImpact').mockResolvedValue(mockResponse);
+    jest.spyOn(component.messageService, 'add');
+    jest.spyOn(component.globalVars.isSavingSection, 'set');
+    await component.saveGenderResearchAndImpactSection();
+    expect(component.api.PATCH_GenderResearchAndImpact).toHaveBeenCalledWith(
+      component.genderResearchAndImpactBody(),
+    );
+    expect(component.messageService.add).toHaveBeenCalledWith({
+      severity: 'error',
+      summary: 'Success',
+      detail: 'Error',
+    });
+    expect(component.globalVars.isSavingSection.set).toHaveBeenCalledWith(false);
   });
 });
